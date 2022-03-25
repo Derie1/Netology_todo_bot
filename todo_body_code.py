62811039e115e901162f4dfdeb5dfc3d7cd8e6b6
Mikhail Davydov <mdavydov83@gmail.com>
2022-03-25 09:59:48 +0300

    added date format check by mask 'yyyy.mm.dd'

--- todo_body_code.py
+++ todo_body_code.py
@@ -5,7 +5,7 @@ show - напечатать все добавленные задачи.
 exit - закончить работу с программой.
 
 """
-
+print(HELP)
 tasks = {
 
 }
@@ -15,36 +15,57 @@ tasks = {
 # Дата -> [Задача1, Задача2, Задача3]
 
 def add_todo(date, task):
-  if date in tasks:
-      # Дата есть в словаре
-      # Добавляем в список задачу
-      tasks[date].append(task)
-  else:
-      # Даты в словаре нет
-      # Создаем запись с ключом date
-      tasks[date] = []
-      tasks[date].append(task)
-  print("Задача ", task, " добавлена на дату ", date)
+    if date in tasks:
+        tasks[date].append(task)
+    else:
+        tasks[date] = []
+        tasks[date].append(task)
+    print("Задача ", task, " добавлена на дату ", date)
+
+def show_by_date(date):
+    if date in tasks:
+        for task in tasks[date]:
+            print('    - ', task)
+    else:
+        print("Такой даты нет.")
+
+def is_correct_date(date):
+    splitted_date = date.split('.')
+    if (len(splitted_date) == 3) and (len(splitted_date[0]) == 4) and (1 <= int(splitted_date[1]) <= 12) and (1 <= int(splitted_date[2]) <= 31):
+        print(f'Вы ввели дату: -== {date} ==-\n')
+        return True
+    else:
+        print('Неверный формат даты.')
+        return False
+
 
 
 run = True
 while run:
-    command = input("Введите команду: ")
+    command = input("Введите команду (или введите exit для выхода): ")
+    
     if command == "help":
         print(HELP)
+    
     elif command == "show":
-        date = input("Введите дату для отображения списка задач: ")
-        if date in tasks:
-            for task in tasks[date]:
-                print('    - ', task)
-        else:
-            print("Такой даты нет")
+        date = input("Введите дату для отображения списка задач (ГГГГ.ММ.ДД): ")
+        while not is_correct_date(date):
+            date = input("Введите дату для отображения списка задач (ГГГГ.ММ.ДД): ")
+        show_by_date(date)
+    
     elif command == "add":
-        date = input("Введите дату для добавления задачи: ")
+        date = input("Введите дату для добавления задачи (ГГГГ.ММ.ДД): ")
+        while not is_correct_date(date):
+            date = input("Введите дату для добавления задачи (ГГГГ.ММ.ДД): ")
         task = input("Введите название задачи: ")
         add_todo(date, task)
-    else: 
-        print("Неизвестная команда")
+    
+    elif:
+        command == 'exit':
         break
+
+    else: 
+        print("Неизвестная команда!\nВведите 'help' для отображения списка команд.")
+        continue
 
 print("До свидания!")
--- todo_bot.py
+++ todo_bot.py
@@ -1,6 +1,9 @@
 import telebot
 
-token = ''
+with open ("bot_token", "r") as myfile:
+    bot_token=myfile.readlines()
+
+token = bot_token[0]
 
 bot = telebot.TeleBot(token)
 
@@ -8,6 +11,5 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=["text"])
 def echo(message):
     bot.send_message(message.chat.id, message.text)
-
 
 bot.polling(none_stop=True)
